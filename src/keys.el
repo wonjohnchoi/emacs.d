98b1f679fe0e42c2510176bf76b035941ea4254b
GunWoo Choi <6566gun@gmail.com>
2015-01-18 16:55:42 +0900

    Use better functions for moving line up-down

--- src/keys.el
+++ src/keys.el
@@ -31,46 +31,24 @@
 (global-set-key (kbd "s-c") 'clipboard-kill-ring-save) ;;copy
 (global-set-key (kbd "s-v") 'clipboard-yank) ;;paste
 
-; code copied from
-; http://stackoverflow.com/questions/2423834/move-line-region-up-and-down-in-emacs
-(defun move-text-internal (arg)
-  "Internal used for `move-text` with ARG."
-  (cond
-   ((and mark-active transient-mark-mode)
-    (if (> (point) (mark))
-        (exchange-point-and-mark))
-    (let ((column (current-column))
-          (text (delete-and-extract-region (point) (mark))))
-      (forward-line arg)
-      (move-to-column column t)
-      (set-mark (point))
-      (insert text)
-      (exchange-point-and-mark)
-      (setq deactivate-mark nil)))
-   (t
-    (let ((column (current-column)))
-      (beginning-of-line)
-      (when (or (> arg 0) (not (bobp)))
-        (forward-line)
-        (when (or (< arg 0) (not (eobp)))
-          (transpose-lines arg))
-        (forward-line -1))
-      (move-to-column column t)))))
+;; http://www.emacswiki.org/emacs/MoveLine
+(defun move-line-up ()
+  "Move line up."
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2))
 
-(defun move-text-down (arg)
-  "Move region (transient-mark-mode active) or current line ARG lines down."
-  (interactive "*p")
-  (move-text-internal arg))
+(defun move-line-down ()
+  "Move line down."
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1))
 
-(defun move-text-up (arg)
-  "Move region (transient-mark-mode active) or current line ARG lines up."
-  (interactive "*p")
-  (move-text-internal (- arg)))
-
-(global-set-key (kbd "M-p") 'move-text-up)
-(global-set-key (kbd "M-n") 'move-text-down)
-(global-set-key (kbd "M-<up>") 'move-text-up)
-(global-set-key (kbd "M-<down>") 'move-text-down)
+(global-set-key (kbd "M-p") 'move-line-up)
+(global-set-key (kbd "M-n") 'move-line-down)
+(global-set-key (kbd "M-<up>") 'move-line-up)
+(global-set-key (kbd "M-<down>") 'move-line-down)
 
 ;; Soft tab
 (setq-default indent-tabs-mode nil)
